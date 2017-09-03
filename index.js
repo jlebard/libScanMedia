1f7090f9481dca9fb98e96fe547e15c99fa48da6
Babar <lescampagnarts@hotmail.fr>
2017-09-03 10:21:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http'); // Fait appel à http.js
+var url = require('url'); // Fait appel à url.js
+
+exports.printMsg = function() {
+	console.log("This is a message from the demo package");
+}
