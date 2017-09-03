daf6eacb5c888eab6c3e4ec38d48fc53bb4734d0
Johann <johann.lebard@free.fr>
2017-09-03 09:55:18 +0000

    gh-pages update

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 #! /usr/bin/env node
 var shell = require("shelljs");
 
 shell.exec("github-pages-commit && github-pages-push");
-=======
-#! /usr/bin/env node
-var shell = require("shelljs");
-
-shell.exec("github-pages-commit && github-pages-push");
->>>>>>> 4602cc541fbd552dccd66649f48a7d8e653431bd
--- bin/push.js
+++ bin/push.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 #! /usr/bin/env node
 var shell = require("shelljs");
-
-=======
-#! /usr/bin/env node
-var shell = require("shelljs");
-
->>>>>>> 4602cc541fbd552dccd66649f48a7d8e653431bd
 shell.exec("git push origin master --force");
